7202693913f5032b02225a156dd8533895a04e2d
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2022-09-22 13:55:45 -0400

    Add transaction

---
+++ src/Wimm/Posting.hs
@@ -0,0 +1,32 @@
+-- |
+-- Module      :  Wimm.Posting
+-- Copyright   :  © 2022 Vincent Archambault
+-- License     :  MIT
+--
+-- Maintainer  :  Vincent Archambault <vincentarchambault@icloud.com>
+-- Stability   :  experimental
+--
+-- This module defines what a posting is.
+module Wimm.Posting
+    ( 
+      Posting(..)
+    ) where
+
+import Data.Time (Day)
+import Data.Aeson (ToJSON, FromJSON, toEncoding, genericToEncoding, defaultOptions)
+import GHC.Generics (Generic)
+import qualified Data.Text as T
+
+-- | The Posting data type reprensents the change in the balance of an account.
+-- Transactions are made of at least two postings.
+data Posting = Posting
+  {
+    pBalanceDate :: Maybe Day, -- The date to consider when computing balance assertion
+    pAccount :: T.Text, -- The account identifier
+    pAmount :: Int -- The amount
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Posting where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON Posting
---
+++ src/Wimm/Transaction.hs
@@ -0,0 +1,32 @@
+-- |
+-- Module      :  Wimm.Transaction
+-- Copyright   :  © 2022 Vincent Archambault
+-- License     :  MIT
+--
+-- Maintainer  :  Vincent Archambault <vincentarchambault@icloud.com>
+-- Stability   :  experimental
+--
+-- This module defines what a transaction is.
+module Wimm.Transaction
+    ( 
+      Transaction(..)
+    ) where
+
+import Data.Time (Day)
+import Data.Aeson (ToJSON, FromJSON, toEncoding, genericToEncoding, defaultOptions)
+import GHC.Generics (Generic)
+import qualified Data.Text as T
+import Wimm.Posting
+
+data Transaction = Transaction
+  {
+    tDate :: Day,
+    tCounterParty :: T.Text,
+    tTags :: [T.Text],
+    tPostings :: [Posting]
+  } deriving (Eq, Show, Generic)
+
+instance ToJSON Transaction where
+    toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON Transaction
