fa4310ef9df9b5ff39b4b5d0de9667bb3813037f
Vincent Archambault <vincentarchambault@icloud.com>
2022-09-22 22:23:19 -0400

    Add tags to transaction reports

--- src/Wimm/Report/Transactions.hs
+++ src/Wimm/Report/Transactions.hs
@@ -20,7 +20,7 @@ import Wimm.Journal
 
 transactionReport :: ReportPeriod -> Journal -> Report
 transactionReport _ journal =
-  let header = ["Id","Date","Account","Account number","Amount", "Counterparty","Comment"]
+  let header = ["Id","Date","Account","Account number","Amount", "Counterparty","Comment","Tags"]
       numberedTxns = zip [1..] (jTransactions journal) :: [(Int, Transaction)]
   in header : foldr serializeTxn [] numberedTxns
 
@@ -30,8 +30,9 @@ transactionReport _ journal =
               date = T.pack $ toISO8601 $ tDate txn
               ctp = tCounterParty txn
               comment = tComment txn
-          in foldr (serializePosting no date ctp comment) [] (tPostings txn) ++ report
+              tags = T.intercalate "|" $ tTags txn
+          in foldr (serializePosting no date ctp comment tags) [] (tPostings txn) ++ report
 
-        serializePosting :: T.Text -> T.Text -> T.Text -> T.Text -> Posting -> Report -> Report
-        serializePosting no date ctp comment p report =
-          [no,date,pAccount p,"",showAmount (jCurrency journal) (pAmount p),ctp,comment] : report
+        serializePosting :: T.Text -> T.Text -> T.Text -> T.Text -> T.Text -> Posting -> Report -> Report
+        serializePosting no date ctp comment tags p report =
+          [no,date,pAccount p,"",showAmount (jCurrency journal) (pAmount p),ctp,comment,tags] : report
