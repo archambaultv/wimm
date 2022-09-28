e90b0d65428ebfb9f5d34c5f7a339e6cd198d748
Vincent Archambault-Bouffard <vincentarchambault@icloud.com>
2022-09-28 11:21:25 -0400

    More integrety check.
    - Each transaction has valid account identifiers
    - Each transaction has at least 2 postings
    - Each transaction balances to zero
    - Each balance assertion has a valid account identifier
    - Each balance assertion is valid
    - Default budget is a valid budget name
    - Each budget has a unique name
    - Each budget has valid account identifiers

--- src/Wimm/Journal.hs
+++ src/Wimm/Journal.hs
@@ -32,6 +32,8 @@ import Wimm.Journal.ReportParameters
 import Wimm.Journal.Budget
 import Wimm.Journal.BalanceAssertion
 
+import qualified Data.HashSet as HS
+import Data.List (intercalate, sort, group)
 import qualified Data.Text as T
 import Data.Maybe (fromMaybe)
 import Data.Function (on)
@@ -42,27 +44,86 @@ import qualified Data.HashMap.Strict as HM
 
 -- | Checks if the integrety of the journal file
 -- Checks that :
+--   - The account identifiers are unique
+--   - The account numbers are unique
 --   - The first fiscal month is valid
 --   - Declared opening balance account is an actual account
 --   - Declared earnings account is an actual account
---   - Each transaction must balance
---   - Balance assertions are valid
+--   - Each transaction has valid account identifiers
+--   - Each transaction has at least 2 postings
+--   - Each transaction balances to zero
+--   - Each balance assertion has a valid account identifier
+--   - Each balance assertion is valid
+--   - Default budget is a valid budget name
+--   - Each budget has a unique name
+--   - Each budget has valid account identifiers
 journalCheck :: Journal -> Either String ()
 journalCheck j = do
+  checkAccounts j
   checkFirstFiscalMonth j
   checkOpeningBalanceAccountExists j
   checkEarningsAccountExists j
-  checkTransactionsBalance j
+  checkTransactions j
   checkBalanceAssertion j
-  -- TODO
+  checkBudget j
 
-  -- check account identifiers are unique
-  -- check account numbers are unique
-  -- check identifier are valid in txns
-  -- check each transactions have at least 2 postings
-  -- check identifier are valid in balance assertion
-  -- check identifier are valid in budget
-  -- check default budget is a valid budget name
+checkBudget :: Journal -> Either String ()
+checkBudget j = do
+  checkBudgetNamesUnique
+  checkDefaultBudget
+  checkBudgetIdentifiers
+
+  where checkDefaultBudget :: Either String ()
+        checkDefaultBudget =
+          case jDefaultBudgetM j of
+            Nothing -> return () -- Nothing to check
+            Just n ->
+              if any ((n ==) . bName) (jBudgets j)
+              then return ()
+              else Left $ "default budget '" ++ T.unpack n ++  "' is not a valid budget name."
+
+        checkBudgetNamesUnique :: Either String ()
+        checkBudgetNamesUnique = noDup "budget names" T.unpack (map bName $ jBudgets j)
+
+        checkBudgetIdentifiers :: Either String ()
+        checkBudgetIdentifiers = 
+          let go :: (Int, Budget) -> Either String ()
+              go (n, b) =
+                case filter (not . flip HS.member accHS) (bAccounts b) of
+                  [] -> return ()
+                  bads ->               
+                    let badsList :: String
+                        badsList = intercalate ", "
+                                $ map (\i -> "'" ++ T.unpack i ++ "'") bads
+                    in Left $ "budget[" ++ show n ++ "] unknown identifiers : " 
+                            ++ badsList
+
+          in traverse_ go (zip [0..] $ jBudgets j)
+
+
+        accHS :: HS.HashSet Identifier
+        accHS = HS.fromList $ map fst $ accInfoList j
+
+        
+
+noDup :: (Ord a) => String -> (a -> String) -> [a] -> Either String ()
+noDup name toStr xs = 
+  case filter (not . null . tail) (group $ sort xs) of
+    [] -> return ()
+    bads -> 
+      let badsList :: String
+          badsList = intercalate ", "
+                    $ map (\l -> "'" ++ toStr (head l) ++ "'") bads
+      in Left $ "duplicate " ++ name ++ " : " ++ badsList
+
+checkAccounts :: Journal -> Either String ()
+checkAccounts j =
+  let accs = accInfoList j
+      idents = map fst accs
+      numbers = map (aiNumber . snd) accs
+  in do
+    noDup "account identifiers" T.unpack idents
+    noDup "account numbers" show numbers
 
 checkFirstFiscalMonth :: Journal -> Either String ()
 checkFirstFiscalMonth j = 
@@ -86,13 +147,40 @@ checkAccIdent foo errMsg j =
      else Left $ errMsg ++ "'" ++ (T.unpack acc)
                ++ "' is not a valid account identifier."
 
-checkTransactionsBalance :: Journal -> Either String ()
-checkTransactionsBalance j = traverse_ checkTxn txns
+checkTransactions :: Journal -> Either String ()
+checkTransactions j = traverse_ runChecks txns
   where txns :: [(Int, Transaction)]
         txns = zip [0..] (jTransactions j)
 
-        checkTxn :: (Int, Transaction) -> Either String ()
-        checkTxn (n, t) = 
+        accHS :: HS.HashSet Identifier
+        accHS = HS.fromList $ map fst $ accInfoList j
+
+        runChecks :: (Int, Transaction) -> Either String ()
+        runChecks t = do
+          checkIdent t
+          checkPostingsAmount t
+          checkBal t
+
+        checkIdent :: (Int, Transaction) -> Either String ()
+        checkIdent (n, t) = 
+          case filter (not . flip HS.member accHS . pAccount) (tPostings t) of
+            [] -> return ()
+            bads -> 
+              let badsList :: String
+                  badsList = intercalate ", "
+                           $ map (\p -> "'" ++ T.unpack (pAccount p) ++ "'") bads
+              in Left $ "transaction[" ++ show n ++ "] unknown identifiers : " 
+                      ++ badsList
+
+        checkPostingsAmount :: (Int, Transaction) -> Either String ()
+        checkPostingsAmount (n, t) = 
+          let ps = tPostings t
+          in if null ps || null (tail ps)
+             then Left $ "transaction[" ++ show n ++ "] has less than 2 postings"
+             else return ()
+
+        checkBal :: (Int, Transaction) -> Either String ()
+        checkBal (n, t) = 
           let txnBalance = sum
                          $ map pAmount
                          $ tPostings t
@@ -103,7 +191,7 @@ checkTransactionsBalance j = traverse_ checkTxn txns
                        ++ "\nActual balance " ++ show txnBalance
 
 checkBalanceAssertion :: Journal -> Either String ()
-checkBalanceAssertion j = traverse_ checkBalance balAssertByIdent
+checkBalanceAssertion j = traverse_ runCheck balAssertByIdent
   where
       -- We will proceed one account at a time,
       -- in increasing order of dates. We keep track
@@ -116,6 +204,14 @@ checkBalanceAssertion j = traverse_ checkBalance balAssertByIdent
                        $ zip [0..] 
                        $ jBalanceAssertions j
 
+      accHS :: HS.HashSet Identifier
+      accHS = HS.fromList $ map fst $ accInfoList j
+
+      runCheck :: [(Int, BalanceAssertion)] -> Either String ()
+      runCheck t = do
+        checkIdent t
+        checkBalance t
+
       -- All the postings related to an account in increasing order of dates
       -- When provided, we use the balance date instead of the transaction date
       postingsMap :: HM.HashMap Identifier [(Day, Posting)]
@@ -126,6 +222,17 @@ checkBalanceAssertion j = traverse_ checkBalance balAssertByIdent
                   $ map (\(t, p) -> (fromMaybe (tDate t) (pBalanceDate p), p)) -- [(Day, Posting)]
                   $ concatMap txnToPostings -- [(Transaction, Posting)]
                   $ jTransactions j
+
+      -- check identifier are valid in balance assertion
+      -- assuming all balance assertions in the list have the same identifier
+      checkIdent :: [(Int, BalanceAssertion)] -> Either String ()
+      checkIdent [] = return ()
+      checkIdent bs@((_,x):_) =
+        if not $ HS.member (baAccount x) accHS
+        then let bads = intercalate ", " $ map (show . fst) bs
+             in Left $ "balance assertion[" ++ bads 
+                      ++ "] unknown identifier : " ++ T.unpack (baAccount x)
+        else return ()
 
       -- Check the validity of a set of balance assertions all related to the
       -- same account. The list must be in increasing order of dates. Simply
